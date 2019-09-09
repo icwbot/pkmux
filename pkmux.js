c563fd96e2ea4c3d4a97f9eee95750413521217f
pardeepsingh588225882 <52146747+pardeepsingh588225882@users.noreply.github.com>
2019-09-09 08:45:37 +0530

    Update pkmux.js

--- pkmux.js
+++ pkmux.js
@@ -453,7 +453,8 @@ var playSong = function(message, connection) {
     if (currentSong) {
         var stream = ytdl(currentSong.url, { "filter": "audioonly", "quality": "lowest" });
         dispatcher = connection.playStream(stream, { volume: serverQueue.volume[message.guild.id] /80 });
-        message.channel.send("server dispatch volume is " + serverQueue.volume[message.guild.id])
+        message.channel.send("server dispatch volume is " + serverQueue.volume[message.guild.id] );
+        message.channel.send("songQueue volume is " + songQueue.volume[message.guild.id]):
         var nowplayembed = new Discord.RichEmbed()
             .setColor(randomcolor)
             .setAuthor(`Now ${(shuffle) ? "randomly " : ""}playing \`${currentSong.title}\``, "https://cdn.discordapp.com/attachments/398789265900830760/405592021579989003/videotogif_2018.01.24_10.46.57.gif")
